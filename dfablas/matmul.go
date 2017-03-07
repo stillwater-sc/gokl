93656b4a98ec8b59c5a2438ceaae286390876911
Ravenwater <theo@stillwater-sc.com>
2017-03-07 09:15:34 -0500

    Getting a working example using gonum/blas: trick was in installing dependent packages via go install github.com/gonum/blas/cgo

--- dfablas/matmul.go
+++ dfablas/matmul.go
@@ -1,5 +1,5 @@
 /*
- File		:	$File: //depot/stillwater-sc/gokl/blas/matmul.go $
+ File		:	$File: //depot/stillwater-sc/gokl/dfa-blas/matmul.go $
 
  Authors	:	E. Theodore L. Omtzigt
  Date		:	6 April 2014
@@ -7,7 +7,7 @@
  Source Control Information:
  Version	:	$Revision: #1 $
  Latest		:	$Date: 2014/04/06 $
- Location	:	$Id: //depot/stillwater-sc/gokl/blas/matmul.go#1 $
+ Location	:	$Id: //depot/stillwater-sc/gokl/dfa-blas/matmul.go#1 $
 
  Organization:
 		Stillwater Supercomputing, Inc.
@@ -19,7 +19,7 @@ Copyright (c) 2014-2017 E. Theodore L. Omtzigt.  All rights reserved.
 Licence      : MIT license as defined in this directory
 
  */
-package blas
+package dfablas
 
 import "github.com/stillwater-sc/gokl"
 
